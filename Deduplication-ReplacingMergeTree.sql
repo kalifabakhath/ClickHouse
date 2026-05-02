244b7f33998d9bc434a220181433e16f470196dc
Mkalifa <kalifabakhath762@gmail.com>
2026-05-02 19:53:03 +0530

    Practiced replacing Merge tree engine

---
+++ Deduplication-ReplacingMergeTree.sql
@@ -0,0 +1,21 @@
+-- Create a table designed to overwrite old data with the same ID
+CREATE TABLE github_events_unique
+(
+    event_id String,
+    event_type String,
+    created_at DateTime,
+    processed_time DateTime
+)
+ENGINE = ReplacingMergeTree(processed_time) -- Uses processed_time to determine the "latest" version
+ORDER BY (event_id);
+
+INSERT INTO github_events_unique 
+SELECT 
+    JSONExtractString(json, 'actor', 'login') AS event_id,
+    JSONExtractString(json, 'type') AS event_type,
+    parseDateTimeBestEffort(JSONExtractString(json, 'created_at')) AS created_at,
+    parseDateTimeBestEffort(JSONExtractString(json, 'created_at')) AS processed_time
+FROM url('https://data.gharchive.org/2024-01-01-15.json.gz', 'JSONAsString');
+
+select count(event_id) from github_events_unique
+describe url('https://data.gharchive.org/2024-01-01-15.json.gz', 'JSONAsString');
